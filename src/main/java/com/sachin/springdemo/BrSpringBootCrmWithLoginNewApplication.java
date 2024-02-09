7274cd35086ef429179ca3301889cc3e6e10941a
Sachin Bansode <sachin.bansode@binaryraptor.com>
2024-02-09 22:38:32 +0530

    Initial Commit

---
+++ src/main/java/com/sachin/springdemo/BrSpringBootCrmWithLoginNewApplication.java
@@ -0,0 +1,13 @@
+package com.sachin.springdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrSpringBootCrmWithLoginNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrSpringBootCrmWithLoginNewApplication.class, args);
+	}
+
+}
